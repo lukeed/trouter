78f38c952b0755b3f4a79a8aad13ef5aeddfbb17
Luke Edwards <luke.edwards05@gmail.com>
2017-11-10 23:09:42 -0800

    import benchmark setup

---
+++ bench/index.js
@@ -0,0 +1,30 @@
+const Table = require('cli-table2');
+const { Suite } = require('benchmark');
+const curr = require('../lib');
+
+const bench = new Suite();
+const args = ['-b', '--bool', '--no-meep', '--multi=baz'];
+
+bench
+	.on('cycle', e => console.log(String(e.target)))
+	.on('complete', function() {
+		console.log('Fastest is ' + this.filter('fastest').map('name'));
+
+		const tbl = new Table({
+			head: ['Name', 'Mean time', 'Ops/sec', 'Diff']
+		});
+
+		let prev, diff;
+
+		this.forEach(el => {
+			if (prev) {
+				diff = ((el.hz - prev) * 100 / prev).toFixed(2) + '% faster';
+			} else {
+				diff = 'N/A'
+			}
+			prev = el.hz;
+			tbl.push([el.name, el.stats.mean, el.hz.toLocaleString(), diff])
+		});
+		console.log(tbl.toString());
+	})
+	.run();
