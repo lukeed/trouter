13a414524b46f2b0cb42e760bd4afb710f91ca91
Luke Edwards <luke.edwards05@gmail.com>
2018-01-11 01:00:50 -0800

    test against ALL http methods; also formatting

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
+const { METHODS } = require('http');
 const { test, Test } = require('tape');
 const Trouter = require('../lib');
 
 const r = new Trouter();
-const METHODS = ['GET', 'POST', 'PUT', 'DELETE'];
 const $ = Test.prototype;
 
 $.isEmpty = function (val, msg) {
@@ -29,23 +29,20 @@ test('exports', t => {
 test('instance', t => {
 	t.true(r instanceof Trouter, 'creates new `Trouter` instances');
 
-	let arr, obj;
 	t.isObject(r.opts, '~> has `opts` key');
 	t.isObject(r.routes, '~> has `routes` key');
 	t.isObject(r.handlers, '~> has `handlers` key');
-	METHODS.forEach(str => {
-		arr=r.routes[str]; obj=r.handlers[str];
-		t.true(Array.isArray(arr), `~~> has \`routes.${str}\` array`);
-		t.is(typeof obj, 'object', `~~> has \`handlers.${str}\` object`);
-		t.is(arr.length, 0, `~~> \`routes.${str}\` is an empty array`);
-		t.is(Object.keys(obj).length, 0, `~~> \`handlers.${str}\` is an empty object`);
-	});
 
-	t.is(typeof r.add, 'function', '~> has `add` function');
-	t.is(typeof r.get, 'function', '~> has `get` function');
-	t.is(typeof r.post, 'function', '~> has `post` function');
-	t.is(typeof r.delete, 'function', '~> has `delete` function');
-	t.is(typeof r.put, 'function', '~> has `put` function');
+	t.isFunction(r.add, '~> has `add` method');
+
+	METHODS.forEach(str => {
+		t.comment(`=== METHOD :: ${str} ===`);
+		t.isFunction(r[str.toLowerCase()], `~> has \`${str}\` method`);
+		t.isArray(r.routes[str], `~~> has \`routes.${str}\` array`);
+		t.isEmpty(r.routes[str], `~~~~> \`routes.${str}\` is an empty array`);
+		t.isObject(r.handlers[str], `~~> has \`handlers.${str}\` object`);
+		t.isEmpty(r.handlers[str], `~~~~> \`handlers.${str}\` is an empty object`);
+	});
 
 	t.end();
 });
