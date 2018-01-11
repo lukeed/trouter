b0a31d4154eccadaea30ce3a21dd7e8655579036
Luke Edwards <luke.edwards05@gmail.com>
2018-01-11 00:40:16 -0800

    bench: add initial test suites

--- bench/index.js
+++ bench/index.js
@@ -1,30 +1,53 @@
 const Table = require('cli-table2');
 const { Suite } = require('benchmark');
-const curr = require('../lib');
+const Trouter = require('../lib');
 
-const bench = new Suite();
-const args = ['-b', '--bool', '--no-meep', '--multi=baz'];
+const routes = [
+	['GET', '/', '/'],
+	['POST', '/users', '/users'],
+	['GET', '/users/:id', '/users/123'],
+	['PUT', '/users/:id/books/:title?', '/users/123/books'],
+	['DELETE', '/users/:id/books/:title', '/users/123/books/foo']
+];
 
-bench
-	.on('cycle', e => console.log(String(e.target)))
-	.on('complete', function() {
-		console.log('Fastest is ' + this.filter('fastest').map('name'));
+const trouter = new Trouter();
 
-		const tbl = new Table({
-			head: ['Name', 'Mean time', 'Ops/sec', 'Diff']
-		});
+routes.forEach(arr => {
+	trouter.add(arr[0], arr[1], noop);
+});
 
-		let prev, diff;
+// Generate & Run all suites
+routes.forEach(arr => {
+	let name = `${arr[0]} ${arr[1]}`;
+	new Suite()
+		.add(name, _ => trouter.find(arr[0], arr[2]))
+		.on('cycle', onCycle)
+		.on('complete', onComplete)
+		.run();
+});
 
-		this.forEach(el => {
-			if (prev) {
-				diff = ((el.hz - prev) * 100 / prev).toFixed(2) + '% faster';
-			} else {
-				diff = 'N/A'
-			}
-			prev = el.hz;
-			tbl.push([el.name, el.stats.mean, el.hz.toLocaleString(), diff])
-		});
-		console.log(tbl.toString());
-	})
-	.run();
+// ~~~~~
+
+function noop() {
+	//
+}
+
+function onCycle(ev) {
+	console.log(String(ev.target));
+}
+
+function onComplete() {
+	console.log('Fastest is ' + this.filter('fastest').map('name'));
+
+	const tbl = new Table({
+		head: ['Name', 'Mean time', 'Ops/sec', 'Diff']
+	});
+
+	let prev, diff;
+	this.forEach(el => {
+		diff = prev ? (((el.hz - prev) * 100 / prev).toFixed(2) + '% faster') : 'N/A';
+		tbl.push([el.name, el.stats.mean, el.hz.toLocaleString(), diff])
+		prev = el.hz;
+	});
+	console.log(tbl.toString());
+}
