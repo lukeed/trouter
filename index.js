6954e46d70b6129dde585aa4638e6b6d0dc75b33
Luke Edwards <luke.edwards05@gmail.com>
2019-03-09 16:08:44 -0800

    feat: add `use()` method;
    - allows non-restrictive URL matching

--- index.js
+++ index.js
@@ -16,27 +16,31 @@ class Trouter {
 		this.put = this.add.bind(this, 'PUT');
 	}
 
+	use(route, ...handlers) {
+		let { keys, pattern } = parse(route, true);
+		this.routes.push({ keys, pattern, method:'', handlers });
+		return this;
+	}
+
 	add(method, route, ...handlers) {
 		let { keys, pattern } = parse(route);
-		let path = '/' + route.match(/^\/?(.*?)\/?(:|\*|$)/)[1];
-		this.routes.push({ keys, pattern, path, method, handlers });
+		this.routes.push({ keys, pattern, method, handlers });
 		return this;
 	}
 
 	find(method, url) {
-		let isALL, isHEAD=(method === 'HEAD');
+		let isHEAD=(method === 'HEAD');
 		let i=0, j=0, tmp, arr=this.routes;
 		let matches=[], params={}, handlers=[];
 		for (; i < arr.length; i++) {
 			tmp = arr[i];
-			isALL = tmp.method.length === 0 && url.indexOf(tmp.path) === 0;
-			if (isALL || tmp.method === method || isHEAD && tmp.method === 'GET') {
+			if (tmp.method.length === 0 || tmp.method === method || isHEAD && tmp.method === 'GET') {
 				if (tmp.keys.length > 0) {
 					matches = tmp.pattern.exec(url);
-					if (matches === null && !isALL) continue;
-					if (matches !== null) for (j=0; j < tmp.keys.length;) params[tmp.keys[j]]=matches[++j];
+					if (matches === null) continue;
+					for (j=0; j < tmp.keys.length;) params[tmp.keys[j]]=matches[++j];
 					tmp.handlers.length > 1 ? (handlers=handlers.concat(tmp.handlers)) : handlers.push(tmp.handlers[0]);
-				} else if (isALL || tmp.pattern.test(url)) {
+				} else if (tmp.pattern.test(url)) {
 					tmp.handlers.length > 1 ? (handlers=handlers.concat(tmp.handlers)) : handlers.push(tmp.handlers[0]);
 				}
 			} // else not a match
